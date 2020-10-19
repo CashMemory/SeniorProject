1a162d20097a00011488f2318cdd5a6078056033
Matthew Cranford <m.cranford13@gmail.com>
2020-10-18 19:52:46 -0600

    pulled out into separate classes

--- tasks/human_pose/get_video.py
+++ tasks/human_pose/get_video.py
@@ -16,126 +16,18 @@ from trt_pose.parse_objects import ParseObjects
 from trt_pose.draw_objects import DrawObjects 
 
 
-class Model:
-    """Deep learning model"""
-
-    def __init__(self, pose_annotations):
-        self.the_model = None
-        self.model_trt = None
-        self.height = None
-        self.width = None 
-        self.model_weights = None
-        self.optimized_model = None
-        self.num_parts = len(pose_annotations["keypoints"])
-        self.num_links = len(pose_annotations["skeleton"])
-
-    def load_model(self, name):
-        """Loads the TensorRT-optimized model which has been pre-trained on
-        the MSCOCO dataset.
-
-        :param name: A string indicating which model to use
-        """
-        # ------------- Constructing the model -------------------------
-        # Each model takes at least two parameters, cmap_channels and
-        # paf_channels corresponding to the number of heatmap channels and part
-        # affinity field channels.
-        #
-        # The number of part affinity field channels is 2x the number of links,
-        # because each link has a channel corresponding to the x and y
-        # direction of the vector field for each link.
-        
-        print("------ model = resnet--------")
-        # resnet18 was trained on an input resolution of 224x224
-        self.width = 224
-        self.height = 224
-        self.model_weights = (
-            "resnet18_baseline_att_224x224_A_epoch_249.pth"
-        )
-        self.optimized_model = (
-            "resnet18_baseline_att_224x224_A_epoch_249_trt.pth"
-        )
-        self.the_model = (
-            trt_pose.models.resnet18_baseline_att(
-                self.num_parts, 2 * self.num_links
-            )
-            .cuda()
-            .eval()
-        )
-        
-
-    def load_weights(self):
-        """Load the model weights.
-
-        Uses two loading functions
-        1) torch.load: Deserializes pickled object files to memory and
-           facilitates the device to load the data into
-        2) load_state_dict: Loads a model's parameter dictionary using a
-           deserialized state_dict
-        (See https://pytorch.org/tutorials/beginner/saving_loading_models.html)
-        """
-        # Load the model weights
-        self.the_model.load_state_dict(torch.load(self.model_weights))
-
-    def get_optimized(self):
-        """Optimize this model by converting Pytorch to TensorRT."""
-        # Create sample data used to optimize with TensorRT
-        self.data = torch.zeros((1, 3, self.height, self.width)).cuda()
-        # Optimize and save results if it's not already optimized
-
-        self.model_trt = TRTModule()
-        self.model_trt.load_state_dict(torch.load(self.optimized_model))
-
-    def log_fps(self):
-        t0 = time.time()
-        torch.cuda.current_stream().synchronize()
-        for i in range(50):
-            _ = self.model_trt(self.data)  # TODO not sure why this necessary
-        torch.cuda.current_stream().synchronize()
-        t1 = time.time()
-        print("FPS log:", 50.0 / (t1 - t0))
-
-    def execute_neural_net(self, data, parser):
-        """Execute the neural network and parse the objects from its output
-
-        :param data:
-        :param parser:
-        :return:
-        """
-        cmap, paf = self.model_trt(data)
-        cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
-        #  cmap_threshold=0.15, link_threshold=0.15)
-        counts, objects, peaks = parser(cmap, paf)
-        return counts, objects, peaks
+from src.camera import Camera
+from src.helper import draw, preprocess, WIDTH, HEIGHT
+from src.model import Model
 
 
-class Camera:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.out = None
-
-    def capture_video(self, codec, output_loc):
-        """
-        :param codec: String representing the video codec (use MP4V)
-        :param output_loc: Location of the output file
-        """
-        fourcc = cv2.VideoWriter_fourcc(*codec)
-        self.out = cv2.VideoWriter(
-            output_loc,
-            fourcc,
-            self.cap.get(cv2.CAP_PROP_FPS),
-            (self.width, self.height)
-        )
 
 
 # Image constants
-WIDTH = 224
-HEIGHT = 224
-X_compress = 640.0 / WIDTH * 1.0
-Y_compress = 480.0 / HEIGHT * 1.0
+#WIDTH = 224
+#$HEIGHT = 224
+#X_compress = 640.0 / WIDTH * 1.0
+#Y_compress = 480.0 / HEIGHT * 1.0
 
 # Image processing constants
 mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
@@ -168,7 +60,6 @@ def main():
     print("Set up model")
 
     # Set up the camera
-    #camera = Camera(width=WIDTH, height=HEIGHT)
     camera = Camera(width=640, height=480)
     camera.capture_video("mp4v", args.output)
     assert camera.cap is not None, "Camera Open Error"
@@ -212,68 +103,6 @@ def main():
     camera.cap.release()
 
 
-def preprocess(image):
-    """Preprocess an image, which is given in BGR8 / HWC format
-
-    NOTE: mean, std, and device are pulled from global scope
-    """
-    global device
-    device = torch.device("cuda")
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = PIL.Image.fromarray(image)
-    image = transforms.functional.to_tensor(image).to(device)  # move to cuda
-    image.sub_(mean[:, None, None]).div_(std[:, None, None])
-    return image[None, ...]
-
-
-def get_keypoint(humans, hnum, peaks):
-    """
-
-    If a particular keypoint is found, it returns a coordinate value between
-    0.0 and 1.0. Multiply this coordinate by the image size to calculate the
-    exact location on the input image
-    :param humans: :param hnum: A 0 based human index
-    :param peaks:
-    :return:
-    """
-    # check invalid human index
-    kpoint = []
-    human = humans[0][hnum]
-    C = human.shape[0]
-    for j in range(C):
-        k = int(human[j])
-        if k >= 0:
-            peak = peaks[0][j][k]  # peak[1]:width, peak[0]:height
-            peak = (j, float(peak[0]), float(peak[1]))
-            kpoint.append(peak)
-        else:
-            peak = (j, None, None)
-            kpoint.append(peak)
-    return kpoint
-
-
-def draw(src, counts, objects, peaks, t):
-    color = (0, 255, 0)  # green
-    fps = 1.0 / (time.time() - t)
-    for i in range(counts[0]):
-        keypoints = get_keypoint(humans=objects, hnum=i, peaks=peaks)
-        for j in range(len(keypoints)):
-            if keypoints[j][1]:
-                x = round(keypoints[j][2] * WIDTH * X_compress)
-                y = round(keypoints[j][1] * HEIGHT * Y_compress)
-                cv2.circle(src, (x, y), 3, color, 2)
-                cv2.putText(src, "%d" % int(keypoints[j][0]), (x + 5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
-                cv2.circle(src, (x, y), 3, color, 2)
-    cv2.putText(
-        src,
-        "FPS: %d" % fps,
-        (20, 20),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        1
-    )
-    return src
 
 
 if __name__ == "__main__":
