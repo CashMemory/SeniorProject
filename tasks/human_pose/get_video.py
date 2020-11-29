029318cb4bbdc790832430f59ad4155bd19c19a3
thehuntforbread <60668195+thehuntforbread@users.noreply.github.com>
2020-11-28 18:59:48 -0700

    full size window
    ya

--- tasks/human_pose/get_video.py
+++ tasks/human_pose/get_video.py
@@ -126,9 +126,11 @@ layout = [
 ]
 
 
-window    = sg.Window("FLAB2AB", layout, 
+window    = sg.Window("FLAB2AB", layout,
                     no_titlebar=False, grab_anywhere=False, 
-                    return_keyboard_events=False, location=(600, 100), finalize=True)        
+                    return_keyboard_events=False, location=(600, 100), finalize=True)    
+
+window.Maximize()   
 
 
 
